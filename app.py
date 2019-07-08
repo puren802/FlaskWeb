3822fe1f84ea8b805bf1037cec34ec33cd6bf192
DongJin Kim <puren802@gmail.com>
2019-07-08 17:31:32 +0900

    second update

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_mysqldb import MySQL
 from data import Articles
 from wtforms import Form,StringField,TextAreaField,PasswordField,validators
 from passlib.hash import sha256_crypt
+from functools import wraps
 
 app=Flask(__name__)
 app.debug=True
@@ -26,6 +27,7 @@ def about():
 @app.route('/articles')
 def afrticles():
     return render_template('articles.html',articles=Articles)
+
 
 @app.route('/articles/<int:id>/')
 def article(id):
@@ -64,15 +66,61 @@ def login():
                 if result>0:
                         data=cur.fetchone()
                         password=data['password']
+
                         if sha256_crypt.verify(password_candidate,password):
-                                app.logger.info('PASSWORD MATCHED')
+                                session['logged_in'] = True
+                                session['username'] = username
+
+                                flash('You are now logged in','success')
+                                return redirect(url_for('dashboard'))
                         else:
-                                error = 'Username not found'
+                                error = 'password not match'
                                 return render_template('login.html',error=error)
+                        cur.close()
                 else:
                         error='Username not found'
                         return render_template('login.html',error=error)                
         return render_template('login.html')
+
+def is_logged_in(f):
+        @wraps(f)
+        def wrap(*args,**kwargs):
+                if 'logged_in' in session:
+                        return f(*args, **kwargs)
+                else:
+                        flash('Unauthorized, please login','danger')
+                return redirect(url_for('login'))
+        return wrap
+
+@app.route('/dashboard')
+@is_logged_in
+def dashboard():
+        return render_template('dashboard.html')
+
+@app.route('/logout')
+def logout():
+        session.clear()
+        flash('you are now logged out','success')
+        return redirect(url_for('login'))
+
+@app.route('/add_article',methods=['GET','POST'])
+@is_logged_in
+def add_article():
+        form = ArticleForm(request.form)
+        if request.method == 'POST' and form.validate():
+                title = form.title.data
+                body = form.body.data
+                cur = mysql.connection.cursor()
+                cur.execute("INSERT INTO articles(title,body,author) VALUES(%s,%s,%s)",(title,body,session['username']))
+                mysql.connection.commit()
+                flash('Article created','success')
+                return redirect(url_for('dashboard'))
+        return render_template('add_article.html',form=form)
+
+class ArticleForm(Form):
+    title = StringField('Title',[validators.Length(min=1,max=50)])
+    body = TextAreaField('Body',[validators.Length(min=30,max=1000)])
+
 
 if __name__ =='__main__':
         app.secret_key='secret123'
